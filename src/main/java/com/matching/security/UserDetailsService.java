f81e94403e767e38b5f77827aec42482fc66bf75
rian <rianbowgift@gmail.com>
2023-01-17 15:04:57 +0900

    [feat] refectoring

--- src/main/java/com/matching/security/UserDetailsService.java
+++ src/main/java/com/matching/security/UserDetailsService.java
@@ -1,4 +1,23 @@
 package com.matching.security;
 
-public class UserDetailsService {
+import com.sparta.blog.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not found user"));
+        return new com.sparta.blog.security.UserDetailsImpl(user, user.getUsername());   //굳이 user.getusername을?
+    }
+
 }
