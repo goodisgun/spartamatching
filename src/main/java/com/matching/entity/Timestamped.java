b5c93ffdbbf658eac8a9ec753c2d1eb9edeb7ce2
kapsin2 <110449509+kapsin2@users.noreply.github.com>
2023-01-17 15:27:45 +0900

    [feat] refactoring

--- src/main/java/com/matching/entity/Timestamped.java
+++ src/main/java/com/matching/entity/Timestamped.java
@@ -1,4 +1,16 @@
 package com.matching.entity;
 
-public class Timestamped {
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+public interface Timestamped {
+
+    @CreatedDate //생성시간 저장
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // 수정시간 저장
+    private LocalDateTime modifiedAt;
+
 }
